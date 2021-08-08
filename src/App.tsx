398c7e471f30b46c1b99ef7c9b4faa99e0330ebe
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2021-08-08 20:23:51 +0200

    feat: Add route for a single user page

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { useRoutes } from "react-router-dom";
 
 import LandingPage from "pages/LandingPage";
 import UsersPage from "pages/UsersPage";
+import UserPage from "pages/UserPage";
 import { navigationList } from "utils/constants";
 
 export default function App() {
     const routes = useRoutes([
         { path: "/", element: <LandingPage navLinks={navigationList} /> },
-        { path: "users", element: <UsersPage /> }
+        { path: "users", element: <UsersPage /> },
+        { path: "users/:id", element: <UserPage /> }
     ]);
     return routes;
 }
