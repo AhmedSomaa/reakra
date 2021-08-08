72c51f98821faea480f789208d22fbb3e395e59e
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2021-08-08 14:54:03 +0200

    feat: Import global stylesheet & inter-ui font stylesi

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import ReactDOM from "react-dom";
 import { App } from "./App";
 import reportWebVitals from "./reportWebVitals";
 import * as serviceWorker from "./serviceWorker";
+import "inter-ui/inter.css";
+import "index.css";
 
 ReactDOM.render(
     <React.StrictMode>
